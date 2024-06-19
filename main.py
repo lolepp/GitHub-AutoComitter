622cdc3b1a4f624298aebc65353aa58e6afd0709
lolepp <rauch-philipp@gmx.de>
2024-06-19 11:36:36 +0200

    70. commit

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def commit_counter():
 
 # Executes git commands
 def main():
-    # With 43 more committs, my repo has 69 committs. This is important!
-    amount = 43 # Enter here how many committs you want
+    # funny number
+    amount = 69 # Enter here how many committs you want
     counter = commit_counter()
     for i in range(amount): # change number of times to commit here
         i += 1
