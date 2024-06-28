daa4c3b7e27802971fc6aa42cd4ec9d8abc2e682
lolepp <rauch-philipp@gmx.de>
2024-06-29 00:06:43 +0200

    1100. commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess
 
 def command(command):
     """
-    Runs a git command using subprocess and prints the output.
+    Runs any command using subprocess and prints the output.
     """
     try:
         result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -13,29 +13,29 @@ def command(command):
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {e}\n{e.stderr}")
 
-def printer(filename):
+def printer(filename): # prints random letter to the given filename
     random_letter = random.choice(string.ascii_letters)  # ascii_letters is lowercase and uppercase
     # Open the file in append mode and add the random letter
     with open(filename, 'a') as file:
         file.write(random_letter + '\n')
 
-def commit_counter():
+def commit_counter(): # Gets the number of committs in this project
     output = command(["git", "rev-list", "--count", "HEAD"])
     return int(output) if output is not None else 0
 
-# Executes git commands
-def main():
-    amount = 68 # Enter here how many committs you want
-    counter = commit_counter()
+def main(): # Executes git commands and the printer
+    counter = commit_counter() # get the number of committs in this project
+    # funny number
+    amount = 69 # Enter here how many committs you want
     filename = 'text.txt' # filename here
     for i in range(amount): # change number of times to commit here
-        i += 1
-        printer(filename) # printer adds random letter to file so there is something to add and commit
-        msg = f"git commit -m \"{i + counter}. commit\""  # commit message here
         branch = "main"  # branch name here
-        command("git add .")
-        command(msg)
-    command("git push origin " + branch)
+        i += 1
+        msg = f"git commit -m \"{i + counter}. commit\""  # commit message here
+        printer(filename) # printer adds random letter to file so there is something to add and commit
+        command("git add .") # git add ., so every change is added
+        command(msg) # commit with the msg
+    command("git push origin " + branch) # git push at the end to shgrten run time
 
 if __name__ == "__main__":
     main()
